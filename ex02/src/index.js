06a90e06e45d90b5596fd983afa3a35286be8f93
AmarMusic91 <music.amar@hotmail.com>
2021-03-10 22:06:23 +0100

    test2

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+  // ovo je test prve  napomene
+  /* ovo je test druge napomene
+  */
+    // Only change code above this line
+
+}
+
+main();
+
+module.exports = main;
